d7cd620c730dbc57965f5c88bb8f3ad1ba09a875
mr_shes <shes-mail@bk.ru>
2018-06-12 14:16:15 +0300

    add no header yield

---
+++ resources/views/dashboard/layouts/scripts.blade.php
@@ -0,0 +1,4 @@
+{{--<link rel="stylesheet" href="{{ asset('css/app.css') }}">--}}
+{{--<script src="{{ asset('js/app.js') }}"></script>--}}
+<link rel="stylesheet" href="/css/app.css">
+<script src="/js/app.js"></script>
