6e3834d2fe4f007437f833e8d794a66050b1ee0a
1kris1 <crist.fedencko2009@yandex.ru>
2018-06-20 23:55:22 +0300

    модальное окно главной страницы

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -10,10 +10,12 @@
                                     <span class="John-Silver">Josh Silver</span>
                                 </div>
                             </div>
-                            <div class="col-md-1 display_none_14 offset-5 text-right p-0"><a href="#" data-toggle="modal" data-target="#settings"><img src="{{ asset('img/icon-setting2.png') }}" alt=""></a></div>
+                            <div class="col-lg-1 offset-lg-5 text-right p-0">
+                                <a href="#" data-toggle="modal" data-target="#settings"><img src="{{ asset('img/icon-setting2.png') }}" alt=""></a>
+                            </div>
                         </div>
                         <div class="row mt-4" >
-                            <div class="col-md-6">
+                            <div class="col-lg-6 col-md-12">
                                 <div class="textarea_pages">
                                     <span>Описание кратное о человеке или сервисе</span>
                                 </div>
@@ -255,9 +257,6 @@
                                                 <div class="form-group">
                                                     <input type="text" class="form-group" placeholder="Create name">
                                                 </div>
-                                                <div class="form-group">
-                                                    <textarea name="" id="" class="form-group" placeholder="Text..."></textarea>
-                                                </div>
                                             </form>
                                         </div>
                                         <div class="col-4 text-right home_edit__button">
@@ -265,6 +264,13 @@
                                             <a href="#"><img src="{{ asset('img/accept-icon.png') }}" alt=""></a>
                                         </div>
                                     </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-group">
+                                            <textarea name="" id="" class="form-group" placeholder="Text..."></textarea>
+                                        </div>
+                                    </div>
+                                </div>
                             </section>
                             <section class="setting_block_background" id="setting_block_background">
                                 <h2 class="setting_block_background__header">Select one of the ready backgrounds</h2>
