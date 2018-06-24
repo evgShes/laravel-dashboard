6e90374e8c661c77d6875a862ddaca0bcaf66025
1kris1 <crist.fedencko2009@yandex.ru>
2018-06-24 17:13:55 +0300

    lp

--- resources/views/lp.blade.php
+++ resources/views/lp.blade.php
@@ -8,7 +8,7 @@
     <title>Document</title>
     @include('dashboard.layouts.scripts')
 </head>
-<body>
+<body class="lp">
 <div class="wrap">
     <section>
         <div class="curr">
@@ -72,42 +72,50 @@
         </div>
     </section>
 </div>
-<header>
+<header class="header_lp" id="header_lp">
     <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand header_lp__logo" href="#"><img src="{{ asset('img/logo_icon.svg') }}" alt=""> REWARDATRON</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#header_lp__navbar" aria-controls="header_lp__navbar" aria-expanded="false">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <div class="collapse navbar-collapse" id="header_lp__navbar">
+            <ul class="navbar-nav m-auto">
+                {{--<li class="nav-item active">--}}
+                    {{--<a class="nav-link" href="#"><img src="{{ asset('img/logo_icon.svg') }}" alt="">REWARDATRON <span class="sr-only">(current)</span></a>--}}
+                {{--</li>--}}
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ADVANTAGES</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
+                    <a class="nav-link" href="#">WE OFFER</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link" href="#">TABLE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">FAQ</a>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+            <ul class="navbar-nav mr-0">
+                <li class="nav-item">
+                    <button class="header_lp__lang" type="button" >ENG</button>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="header_lp__sign">SIGN UP</button>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="header_lp__login">LOGIN</button>
+                </li>
+            </ul>
         </div>
     </nav>
+    <div class="container">
+        <div class="d-flex align-items-end">
+            <img src="" alt="">
+            <span>rewardatron</span>
+        </div>
+    </div>
 </header>
 </body>
 </html>
