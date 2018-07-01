c56c35922b4c2970d6b94a07c25e28f846d2f589
1kris1 <crist.fedencko2009@yandex.ru>
2018-07-01 16:10:11 +0300

    анимация

--- resources/assets/js/scripts/lp.js
+++ resources/assets/js/scripts/lp.js
@@ -13,4 +13,32 @@ jQuery(function ($) {
         }, 800);
         return false;
     });
+    // $('#content_advantages,\n' +
+    //     '#we_offer,\n' +
+    //     '#create_currency,\n' +
+    //     '#block_faq').bind('click', '#header_lp__navbar a,.scroll_bottom', function(event) {
+    //     $.scrollTo(event.target.hash, 250);
+    // });
+    // $('#header_lp__navbar a,.scroll_bottom').on('click',function(){
+    //     console.log($(this).attr('href'));
+    //     $($(this).attr('href')).animate({
+    //
+    //     },800);
+    // });
+    $("#header_lp__navbar ul li a[href^='#'],.scroll_bottom").on('click', function(e) {
+
+        // prevent default anchor click behavior
+        e.preventDefault()
+
+        // store hash
+        var hash = this.hash
+
+        // animate
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash
+        })
+
+    })
 });
